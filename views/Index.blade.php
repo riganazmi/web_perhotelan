caede6e770258823eeb899c345766f8772296a21
Rigan Azmi <93499714+riganazmi@users.noreply.github.com>
2022-01-10 16:42:19 +0700

    Add files via upload

---
+++ views/Index.blade.php
@@ -0,0 +1,60 @@
+@extends('master')
+@section('content')
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/7e/c2/61/hotel-asri-sumedang.jpg?w=900&h=600&s=1">
+            <div class="card-body">
+              <h5 class="card-title">Hotel Asri</h5>
+              <p class="card-text">Terletak di Sumedang, Hotel Asri Sumedang menawarkan akomodasi bintang 3 dengan taman. Akomodasi ini memiliki pusat kebugaran, Wi-Fi gratis, dan resepsionis 24 jam.
+
+Kamar-kamar di hotel ini memiliki AC dan TV layar datar.
+
+Sarapan prasmanan disajikan setiap pagi di Hotel Asri Sumedang.
+
+Akomodasi ini berjarak 48 km dari Bandung. Bandara terdekat adalah Bandara Internasional Kertajati, 32 km dari Hotel Asri Sumedang.
+
+Pasangan paling suka lokasinya — mereka memberi nilai 8,5 untuk perjalanan dua orang.
+
+Hotel Asri Sumedang telah menyambut tamu Booking.com sejak 3 Agu 2017.</p>
+            
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://origin.pegipegi.com/jalan/images/pictM/Y7/Y995907/Y995907003.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Hotel Amory</h5>
+              <p class="card-text"> Sebuah hotel yang berlokasi di tengah kota Sumedang. Berada di pinggir jalan utama yang melewati pusat kota Sumedang. Menyediakan penginapan yang bersih, rapih, bagus dan nyaman. Selain penginapan dilengkapi juga dengan fasilitas ruangan pertemuan (meeting room) yang bisa menampung peserta antara 15 sampai 60 orang dan restoran.
+
+Kelas kamar yang tersedia ada tiga jenis yaitu kamar standar dengan harga 375 ribu, kamar deluks dengan tarif 425 ribu dam kamar super deluks dengan tarif 485 ribu.
+
+Yang mengunjungi Sumedang dan bermaksud menginap, hotel ini bisa menjadi salah satu referensinya.</p>
+
+</div>            
+</div>
+</div>
+</div>
+</div>         
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://sumedangtandang.com/upload/foto/hotel_la_diva.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Hotel Ladiva</h5>
+              <p class="card-text">Merupakan salah satu dari sekian hotel yang ada di kota Sumedang. Hotel La Diva letaknya sangat dekat dengan pusat kota Sumedang. Sekitar 100 meter arah barat Alun-alun Sumedang. Hotel La Diva memiliki citra nuansa Neo Classic & Natural.
+
+La Diva menyajikan hotel penginapan yang nyaman dan representatif di Kabupaten Sumedang dengan total 24 kamar lengkap dengan water heather setiap kamarnya, beauty care & spa, serta area serbaguna. Fasilitas ini bisa digunakan untuk kegiatan meeting maupun pesta pernikahan dengan view pemandangan alami.</p>
+</div>            
+</div>
+</div>
+</div>
+</div>           
+@endsection
